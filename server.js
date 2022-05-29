3e446b0029a9866f15a57ff9ecb747df6c54f47a
mjzabriskie <99937908+mjzabriskie@users.noreply.github.com>
2022-05-28 21:53:12 -0600

    connected the database to the server and ensured the connection works

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express');
-const mysql2 = require('mysql2');
+const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'pass1234',
+        database: 'election'
+    },
+    console.log('Connected to the election database')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+})
 
 app.use((req, res) => {
     res.status(404).end();
