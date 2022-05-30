a1c9aa87150f2b49f102c9bce1f6cf3d77f7d5a7
mjzabriskie <99937908+mjzabriskie@users.noreply.github.com>
2022-05-29 18:38:45 -0600

    add CRUD routes

--- server.js
+++ server.js
@@ -97,17 +97,6 @@ app.post('/api/candidate', ({ body }, res) => {
     });
 });
 
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-//                 VALUES (?,?,?,?)`;
-// const params = [1, 'Ronald', 'Firbank', 1];
-
-// db.query(sql, params, (err, result) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(result);
-// });
-
 app.use((req, res) => {
     res.status(404).end();
 });
